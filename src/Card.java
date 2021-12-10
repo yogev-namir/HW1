d4aeb5029aecb405367994b000db53d5354e80de
TomerGrinberg1 <81254017+TomerGrinberg1@users.noreply.github.com>
2021-12-10 10:41:16 +0200

    Update Card.java

--- src/Card.java
+++ src/Card.java
@@ -4,7 +4,11 @@ public class Card{
     public static final String[] cardType = {"♠","♦","♣","♥"};
     public static final String[] cardValue = {"Ace","2","3","4","5","6","7","8",
                                                 "9","10","Jack","Queen","King"};
-
+/**
+     * Initializes the card as required
+     * @param number: the number of the card
+     * @param shape: the shape of the card
+     */
     public Card(Suit type, int value){
         this.type = type;
         this.value = value;
@@ -16,7 +20,11 @@ public class Card{
     public Suit getType() {
         return type;
     }
-
+/**
+     * Compares 2 cards and checks who is bigger
+     * @param other: object from Card type
+     * @return which of the cards values is higher
+     */
     public int compare(Card other){
         if(other.getValue()!=1 && this.getValue()!=1)
         return Integer.compare(this.getValue(), other.getValue());
@@ -27,7 +35,10 @@ public class Card{
                 return -1;
 
     }
-
+/**
+     * Create a string with the value of the Card and his shape
+     * @return value and  shape of the Card
+     */
     public String toString(){
         return (cardValue[value-1] + " of " + cardType[type.showType()]);
     }
