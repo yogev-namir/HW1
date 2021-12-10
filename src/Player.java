50a1db27201b4b4d32d8a7254c68c8a8931e2a44
TomerGrinberg1 <81254017+TomerGrinberg1@users.noreply.github.com>
2021-12-10 10:49:03 +0200

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -2,6 +2,10 @@ public class Player {
     String name;
     Deck drawDeck, storeDeck;
 
+    /**
+     * initializes 2 package and the name of the player as required
+     * @param name: the name of the player
+     */
     public Player(String name){
         this.name = name;
         drawDeck = new Deck(false);
@@ -20,12 +24,25 @@ public class Player {
 
     }
     */
-
+ /**
+     * draw a card
+     * @return: the card we wand to draw
+     */
     public Card drawCard() {return drawDeck.removeTopCard();}
 
+    /**
+     * check if the 2 pack is empty
+     * @return : true if the 2 pack is empty, otherwise false
+     */
+   
+         
     public boolean outOfCards(){
         return drawDeck.isEmpty() && storeDeck.isEmpty();
     }
+     /**
+     * return the name of the player
+     * @return : the name of the player
+     */
     public String toString() {return this.name;}
 
 }
